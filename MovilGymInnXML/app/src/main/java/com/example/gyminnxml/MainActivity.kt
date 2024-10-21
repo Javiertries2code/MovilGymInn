71ad13024ce16aecfc67a2a5899d60f4d1216ea7
in2dm3-v <in2dm3-v@elorrieta.int>
2024-10-21 15:33:43 +0200

    Commit de el fichero register.xml

--- MovilGymInnXML/app/src/main/java/com/example/gyminnxml/MainActivity.kt
+++ MovilGymInnXML/app/src/main/java/com/example/gyminnxml/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.example.gyminnxml
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btnRegistro = findViewById<Button>(R.id.btnRegistro)
 
-
-
+        btnRegistro.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
-
-
-
 }
