fffa61505421a992e28a062e7c425d9c614d2bc5
dayanaandrea <dayanaandrea.fernandezla@elorrieta-errekamari.com>
2024-10-23 20:31:14 +0200

    Commit creacion de pantallas
    Creamos las vistas de casi todas las pantallas y añadimos algo de funcionalidad

--- MovilGymInnXML/app/src/main/java/com/example/gyminnxml/WorkOutsActivity.kt
+++ MovilGymInnXML/app/src/main/java/com/example/gyminnxml/WorkOutsActivity.kt
@@ -1,12 +1,26 @@
 package com.example.gyminnxml
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class WorkOutsActivity  : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_workouts)
+
+        val imagenProfile = findViewById<ImageView>(R.id.imageView3)
+        imagenProfile.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+        val buttonVolver = findViewById<Button>(R.id.buttonVolverWorkouts)
+        buttonVolver.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
