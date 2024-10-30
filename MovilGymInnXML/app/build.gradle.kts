203a7680e1427b3c8be812f7d3db6d1d4c1ef819
Youssef <youssef.alha@elorrieta-errekamari.com>
2024-10-30 18:48:12 +0100

    Arreglamos conflicto

--- MovilGymInnXML/app/build.gradle.kts
+++ MovilGymInnXML/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
+
 }
 
 android {
@@ -42,6 +44,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.inappmessaging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- MovilGymInnXML/app/src/main/java/com/example/gyminnxml/ProfileActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.gyminnxml
-
-import androidx.appcompat.app.AppCompatActivity
-
-class ProfileActivity :  AppCompatActivity() {
-
-}
